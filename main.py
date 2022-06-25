156133f65fd06022ad6f7886963db4d59898753f
SQRT676 <sumitpandey026@gmail.com>
2022-06-25 20:39:24 +0530

    test01

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("testing")
-print("check")
+def printBoard():
+    print(f" 0 | 1 | 2 ")
+    print("---------------")
+    print(f" 3 | 4 | 5 ")
+    print("---------------")
+    print(f" 6 | 7 | 8 ")
+
+
+if __name__=="__main__":
+    xstate = [0, 0, 0, 0, 0, 0, 0, 0]
+    zstate = [0, 0, 0, 0, 0, 0, 0, 0]
+    turn=1
+    print("Welcome to Tic Tac Toe")
+    while(True):
+        printBoard()
+        if(turn==1):
+            print("X's chance")
+            value = int(input("Plz enter a val"))
+            xstate[value] = 1
+        else:
+            print("X's chance")
+            value = int(input("Plz enter a val"))
+            xstate[value] = 1
